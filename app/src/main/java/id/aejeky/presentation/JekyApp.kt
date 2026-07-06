48065a973987cb93994c56ae56346ef13f944c27
Tria Sinta <sjtaaa.0jj@gmail.com>
2026-07-06 15:19:41 +0700

    Update redirect untuk firstscreen dan loginscreen

--- app/src/main/java/id/aejeky/presentation/JekyApp.kt
+++ app/src/main/java/id/aejeky/presentation/JekyApp.kt
@@ -6,25 +6,55 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import id.aejeky.data.model.Layanan
+import id.aejeky.presentation.screen.FirstScreen
 import id.aejeky.presentation.screen.HomeScreen
+import id.aejeky.presentation.screen.LoginScreen
 import id.aejeky.presentation.screen.OrderScreen
 
 @Composable
 fun JekyApp() {
+    var currentScreen by remember { mutableStateOf("first") }
     var selectedService by remember { mutableStateOf<Layanan?>(null) }
 
-    if (selectedService == null) {
-        HomeScreen(
-            onServiceClick = { service ->
-                selectedService = service
+    when (currentScreen) {
+        "first" -> {
+            FirstScreen(
+                onStartClick = {
+                    currentScreen = "login"
+                }
+            )
+        }
+
+        "login" -> {
+            LoginScreen(
+                onBackClick = {
+                    currentScreen = "first"
+                },
+                onLoginClick = {
+                    currentScreen = "home"
+                }
+            )
+        }
+
+        "home" -> {
+            HomeScreen(
+                onServiceClick = { service ->
+                    selectedService = service
+                    currentScreen = "order"
+                }
+            )
+        }
+
+        "order" -> {
+            selectedService?.let { service ->
+                OrderScreen(
+                    service = service,
+                    onBackClick = {
+                        selectedService = null
+                        currentScreen = "home"
+                    }
+                )
             }
-        )
-    } else {
-        OrderScreen(
-            service = selectedService!!,
-            onBackClick = {
-                selectedService = null
-            }
-        )
+        }
     }
 }
