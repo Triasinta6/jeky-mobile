4066babf3fd580ccd47dd61f0fce39d19da29a0d
Tria Sinta <sjtaaa.0jj@gmail.com>
2026-07-08 09:37:44 +0700

    Add package theme

---
+++ app/src/main/java/id/aejeky/presentation/theme/Color.kt
@@ -0,0 +1,27 @@
+package id.aejeky.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF2563EB)
+val DarkBlue = Color(0xFF0B63F6)
+val LightBlue = Color(0xFF60A5FA)
+
+val ScreenBackground = Color(0xFFF7F7F7)
+val SoftBlueBackground = Color(0xFFEFF6FF)
+
+val White = Color(0xFFFFFFFF)
+val TextPrimary = Color(0xFF101828)
+val TextSecondary = Color(0xFF667085)
+val TextPlaceholder = Color(0xFF98A2B3)
+
+val SuccessGreen = Color(0xFF12B76A)
+val PriceGreen = Color(0xFF4CAF50)
+val ErrorRed = Color(0xFFF04438)
+
+val BorderGray = Color(0xFFEFEFEF)
+val IconGray = Color(0xFFB5B5B5)
+val SearchIconGray = Color(0xFF344054)
+
+val ServiceFoodOrange = Color(0xFFF97316)
+val ServiceSendGreen = Color(0xFF039855)
+val ServiceMartPurple = Color(0xFF7C3AED)
